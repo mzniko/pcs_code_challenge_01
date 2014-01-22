6ca031a085ea787b65b293009ad68c499dff1363
Marko Nikolovski <mz.nikolovski@gmail.com>
2014-01-21 21:10:18 -0800

    rubocopification and minor stylistic refactoring

--- madlibs.rb
+++ madlibs.rb
@@ -1,44 +1,38 @@
 require 'pry'
 
 def parts
-  puts "Please enter a noun or noun phrase:"
+  puts 'Please enter a noun or noun phrase:'
   noun_input = gets.chomp
-
-  puts "Please enter an adjective or adjectival phrase:"
+  puts 'Please enter an adjective or adjectival phrase:'
   adj_input = gets.chomp
-
-  puts "Please enter a verb or verb phrase:"
+  puts 'Please enter a verb or verb phrase:'
   verb_input = gets.chomp
-
-  puts "Please enter an adverb or adverbial phrase:"
+  puts 'Please enter an adverb or adverbial phrase:'
   adv_input = gets.chomp
-
-  puts "Please enter an object:"
+  puts 'Please enter an object:'
   obj_input = gets.chomp
 
   gathered_parts = { noun: noun_input, adj: adj_input, verb: verb_input,
                      adv: adv_input, obj: obj_input }
- 
   binding.pry
-
   gathered_parts
 end
 
-def template gathered_parts
-  
-  template_a = "The #{gathered_parts[:adj]} #{gathered_parts[:noun]} suddenly and #{gathered_parts[:adv]} had to #{gathered_parts[:verb]} the #{gathered_parts[:obj]}."
+def template(gathered_parts)
+  template_a = "The #{gathered_parts[:adj]} #{gathered_parts[:noun]} suddenly \
+and #{gathered_parts[:adv]} had to #{gathered_parts[:verb]} the \
+#{gathered_parts[:obj]}."
+  template_b = "A #{gathered_parts[:adj]} #{gathered_parts[:noun]} likes \
+#{gathered_parts[:obj]}."
+  template_c = "We #{gathered_parts[:verb]}ed #{gathered_parts[:adv]} to get \
+the #{gathered_parts[:noun]}."
 
-  template_b = "A #{gathered_parts[:adj]} #{gathered_parts[:noun]} likes #{gathered_parts[:obj]}."
-  
-  template_c = "We #{gathered_parts[:verb]}ed #{gathered_parts[:adv]} to get the #{gathered_parts[:noun]}."
-  
   templates = [template_a, template_b, template_c]
 
-  templates.each { |template|
+  templates.each do |template|
     puts template
     binding.pry
-  }
-
+  end
 end
 
 template parts
